8277e85f67c378d5f2bf4f360ea298b56225332d
denyskh4991 <denyskh4991@gmail.com>
2024-08-31 21:25:06 +0200

    Adding function to show and hide message

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ let pass = document.getElementById("password");
 let msg = document.getElementById("message");
 let str = document.getElementById("strength");
 
+pass.addEventListener('input', () => {
+    if (pass.value.length > 0) {
+        msg.style.display = "block";
+    } else {
+        msg.style.display = "none";
+    }
+})
